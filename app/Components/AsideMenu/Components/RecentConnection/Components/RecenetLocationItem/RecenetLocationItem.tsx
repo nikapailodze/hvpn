d62f15668e0419ecf15bb15185959d3d95b7fa7b
nikapailodze <nikapail.a01@gmail.com>
2024-11-17 17:32:06 +0400

    refeat: addded checkbox functionality and design

--- app/Components/AsideMenu/Components/RecentConnection/Components/RecenetLocationItem/RecenetLocationItem.tsx
+++ app/Components/AsideMenu/Components/RecentConnection/Components/RecenetLocationItem/RecenetLocationItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './RecenetLocationItem.module.scss'
 import Image from 'next/image';
 
@@ -8,6 +9,13 @@ interface Props {
 }
 
 const RecenetLocationItem = (props: Props) => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const onCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsChecked(event.target.checked);
+    };
+
+
     return (
         <div className={styles.container}>
             <div className={styles.content}>
@@ -16,7 +24,17 @@ const RecenetLocationItem = (props: Props) => {
                     <Image src={'/Icons/msBlue.svg'} width={24} height={24} alt='speed in milliseconds' />
                     <p>107 Ms</p>
                 </div>
-                <input className={styles.checkBox} type="checkbox" />
+                <div className={styles.checkBoxWrapper}>
+                    <span
+                        className={`${styles.checkBoxWrapperFiller} ${isChecked ? styles.filled : ""}`}
+                    ></span>
+                    <input
+                        checked={isChecked}
+                        onChange={onCheckboxChange} // Handle change here
+                        className={styles.checkBox}
+                        type="checkbox"
+                    />
+                </div>
             </div>
         </div>
     )
